809efdf04a2c5171b5ea3176aecaafd8e5bf2407
fabioskb <fabiotest64@gmail.com>
2022-05-01 10:37:09 -0300

    clean code

--- src/metodos/YoutubeArquivo.java
+++ src/metodos/YoutubeArquivo.java
@@ -4,15 +4,11 @@
  */
 package metodos;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Formatter;
 import java.util.List;
 
 /**
--- src/metodos/YoutubeText.java
+++ src/metodos/YoutubeText.java
@@ -3,38 +3,38 @@ package metodos;
 import aplicacao.tela.YoutubeTela;
 
 public class YoutubeText {
-
     private String idioma;
     
     public YoutubeText(String idioma) {
         this.idioma = idioma;
     }
 
-
     /**
      * Retorna o texto, traduzido, de acordo com o idioma do sistema.
      *
-     * @param key (String) - KEYS: DESCRIÇÃO-----------> descricao.
+     * @param key (String) - KEYS: 
+     * DESCRIÇÃO-----------> descricao. 
      * BUTTONS-------------> botao.modo.noite, botao.video, botao.audio,
-     * botao.pesquisar, botao.baixar, botao.cancelar. fieldtext.link,
-     * fieldtext.pesquisar. TOOLTIP------------> tooltip.botao.noturno,
-     * tooltip.link, tooltip.pesquisa, tooltip.botao.pesquisa,
-     * tooltip.botao.baixar, tooltip.botao.video, tooltip.botao.audio,
-     * tooltip.lista, tooltip.botao.cancelar, tooltip.label.resultado,
+     * botao.pesquisa, botao.baixar, botao.cancelar. 
+     * FIELDTEXT----------> fieldtext.link, fieldtext.pesquisa. 
+     * TOOLTIP------------> tooltip.botao.noturno, 
+     * tooltip.link, tooltip.pesquisa, tooltip.botao.pesquisa, 
+     * tooltip.botao.baixar, tooltip.botao.video, tooltip.botao.audio, 
+     * tooltip.lista, tooltip.botao.cancelar, tooltip.label.resultado, 
      * tooltip.menuitem.sair, tooltip.menuitem.sobre, tooltip.menuitem.tutorial.
-     * LABEL RESULTADO-----------> label.resultado.pesquisando,
-     * label.resultado.baixando, label.resultado.falha.pesquisa,
-     * label.resultado.falha.download, label.resultado.aviso.item,
-     * label.resultado.aviso.video, label.resultado.download.concluido,
-     * label.resultado.pesquisa.concluida, label.resultado.aviso.ja.baixado,
+     * LABEL RESULTADO-----------> label.resultado.pesquisando, 
+     * label.resultado.baixando, label.resultado.falha.pesquisa, 
+     * label.resultado.falha.download, label.resultado.aviso.item, 
+     * label.resultado.aviso.video, label.resultado.download.concluido, 
+     * label.resultado.pesquisa.concluida, label.resultado.aviso.ja.baixado, 
      * label.resultado.download.cancelado, label.resultado.verificando.download.
-     * JPTIONPANE----------> joptionpane.boas.vndas,
-     * joptionpane.erro.dependencias, joptionpane.instalacao.concluida,
-     * joptionpane.pip.nao.instalado, joptionpane.tutorial, joptionpane.sobre,
-     * joptionpane.sobre.titulo, joptionpane.sair.pergunta,
-     * joptionpane.botao.oksair, joptionpane.botao.cancelar.
-     * PROGRESSBAR----------> progressbar.baixando.audio. MENU ITEM----------->
-     * menuitem.sobre, menuitem.sair, menuitem.tutorial.
+     * JPTIONPANE----------> joptionpane.boas.vndas, 
+     * joptionpane.erro.dependencias, joptionpane.instalacao.concluida, 
+     * joptionpane.pip.nao.instalado, joptionpane.tutorial, joptionpane.sobre, 
+     * joptionpane.sobre.titulo, joptionpane.sair.pergunta, 
+     * joptionpane.botao.oksair, joptionpane.botao.cancelar. 
+     * PROGRESSBAR----------> progressbar.baixando.audio. 
+     * MENU ITEM-----------> menuitem.sobre, menuitem.sair, menuitem.tutorial.
      * @return texto
      */
     public String pegarTexto(String key) {
